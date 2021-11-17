6fc97ee4bdef248a175cf35159ed9d12ad1bb5ab
Michael Buzzard <m.buzzard87@gmail.com>
2021-11-17 17:49:13 -0500

    add InsertUniversities method and DataContext

--- LingToSQL/MainWindow.xaml.cs
+++ LingToSQL/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
 
 namespace LingToSQL
 {
@@ -20,9 +21,37 @@ namespace LingToSQL
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        LinqToSQLDataClassesDataContext dataContext;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["LingToSQL.Properties.Settings.BuzzyDBConnectionString"].ConnectionString;
+            dataContext = new LinqToSQLDataClassesDataContext(connectionString);
+
+            InsertUniversities();
+
         }
+
+        public void InsertUniversities()
+        {
+            dataContext.ExecuteCommand("delete from University");
+
+            University yale = new University() { Name = "Yale" };
+            dataContext.Universities.InsertOnSubmit(yale);
+
+            University ccc = new University() { Name = "Camden County College" };
+            dataContext.Universities.InsertOnSubmit(ccc);
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Universities;
+        }
+
+
+        
+
     }
 }
